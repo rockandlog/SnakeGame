e44c54c7c71a646948ebfaf6178fdca15b89b54c
rockandlog <aleksandra.jaskolska1@gmail.com>
2025-05-12 22:23:57 +0200

    User1: Dodano wykrywanie kolizji ze ścianą

--- Program.cs
+++ Program.cs
@@ -7,6 +7,19 @@ class Program
     static int height = 20;
     static bool gameRunning = true;
 
+    static ConsoleKey currentDirection = ConsoleKey.RightArrow;
+    static Thread inputThread;
+
+    static int snakeX = width / 2;
+    static int snakeY = height / 2;
+
+    static int foodX;
+    static int foodY;
+
+    static int score = 0;
+
+    static Random rand = new Random();
+
     static void Main()
     {
         Console.CursorVisible = false;
@@ -15,20 +28,27 @@ class Program
 
         InitializeGame();
 
+        inputThread = new Thread(ReadInput);
+        inputThread.Start();
+
         while (gameRunning)
         {
             Update();
-            Thread.Sleep(100); // odświeżanie co 100 ms
+            Thread.Sleep(100);
         }
 
         Console.Clear();
         Console.WriteLine("Game Over!");
+        Console.WriteLine("Twój wynik: " + score);
     }
 
     static void InitializeGame()
     {
         Console.Clear();
         DrawBorder();
+        SpawnFood();
+        DrawSnake();
+        DrawScore();
     }
 
     static void DrawBorder()
@@ -52,9 +72,93 @@ class Program
         }
     }
 
+    static void DrawSnake()
+    {
+        Console.SetCursorPosition(snakeX, snakeY);
+        Console.Write("O");
+    }
+
+    static void ClearSnake()
+    {
+        Console.SetCursorPosition(snakeX, snakeY);
+        Console.Write(" ");
+    }
+
+    static void DrawFood()
+    {
+        Console.SetCursorPosition(foodX, foodY);
+        Console.Write("@");
+    }
+
+    static void SpawnFood()
+    {
+        foodX = rand.Next(1, width - 1);
+        foodY = rand.Next(1, height - 1);
+        DrawFood();
+    }
+
+    static void DrawScore()
+    {
+        Console.SetCursorPosition(2, 0); // przeniesione na górę ekranu
+        Console.Write("Wynik: " + score + "   ");
+    }
+
+    static void ReadInput()
+    {
+        while (gameRunning)
+        {
+            if (Console.KeyAvailable)
+            {
+                var key = Console.ReadKey(true).Key;
+
+                if ((key == ConsoleKey.UpArrow && currentDirection != ConsoleKey.DownArrow) ||
+                    (key == ConsoleKey.DownArrow && currentDirection != ConsoleKey.UpArrow) ||
+                    (key == ConsoleKey.LeftArrow && currentDirection != ConsoleKey.RightArrow) ||
+                    (key == ConsoleKey.RightArrow && currentDirection != ConsoleKey.LeftArrow))
+                {
+                    currentDirection = key;
+                }
+            }
+
+            Thread.Sleep(10);
+        }
+    }
+
     static void Update()
     {
-        // Na razie nic – tu będą działania gracza w przyszłości
-        // (np. poruszanie wężem, rysowanie, kolizje)
+        ClearSnake();
+
+        switch (currentDirection)
+        {
+            case ConsoleKey.UpArrow:
+                snakeY--;
+                break;
+            case ConsoleKey.DownArrow:
+                snakeY++;
+                break;
+            case ConsoleKey.LeftArrow:
+                snakeX--;
+                break;
+            case ConsoleKey.RightArrow:
+                snakeX++;
+                break;
+        }
+
+        // 💥 Kolizja ze ścianą
+        if (snakeX <= 0 || snakeX >= width - 1 || snakeY <= 0 || snakeY >= height - 1)
+        {
+            gameRunning = false;
+            return;
+        }
+
+        // 🍏 Zjedzenie jedzenia
+        if (snakeX == foodX && snakeY == foodY)
+        {
+            score++;
+            SpawnFood();
+            DrawScore();
+        }
+
+        DrawSnake();
     }
 }
